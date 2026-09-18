90f004be6b363ad05a154ec888d942808e73dfa1
XuCai021130 <xucai7140@gmail.com>
2026-09-17 22:44:10 -0400

    add thunk and persist

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -4,8 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.util';
-import { setCategories } from '../../store/categories/category.action';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 import './shop.styles.scss';
 
@@ -14,12 +13,9 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategories = async () => {
-      const categoriesArray = await getCategoriesAndDocuments();
-      dispatch(setCategories(categoriesArray));
-    }
-    getCategories();
+    dispatch(fetchCategoriesAsync());
   }, []);
+  
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
